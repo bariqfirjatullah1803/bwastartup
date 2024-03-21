cab921bce0c4523338cc382952d2439a94f606ca
Bariq Firjatullah <bariqfirjatullah1803@gmail.com>
2024-03-21 21:21:54 +0700

    Initial commit

---
+++ handler/user.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+	"bwastartup/helper"
+	"bwastartup/user"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userService user.Service) *userHandler {
+	return &userHandler{userService}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+	// Get input user
+	// Map input from user to struct RegisterUserInput
+	// Self struct passing to param service
+
+	var input user.RegisterUserInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Register account failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	newUser, err := h.userService.RegisterUser(input)
+	if err != nil {
+		response := helper.APIResponse("Register account failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	formatter := user.FormatUser(newUser, "token token token")
+
+	response := helper.APIResponse("Account has been registered", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+}
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bwastartup/handler"
+	"bwastartup/user"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+)
+
+func main() {
+	dsn := "bariq:bariq123123@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+
+	userHandler := handler.NewUserHandler(userService)
+
+	router := gin.Default()
+	api := router.Group("/api/v1")
+
+	api.POST("/users", userHandler.RegisterUser)
+
+	router.Run()
+}
